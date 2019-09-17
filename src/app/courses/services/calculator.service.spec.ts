73a756423c95f02950c5ed4290bb505dde950704
Calvin Durrant <calvindurramt@Calvins-MacBook-Pro.local>
2019-09-17 13:04:54 +0200

    calculator service testing: using test bed

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,4 +1,6 @@
 import { CalculatorService } from './calculator.service';
+import { TestBed } from '@angular/core/testing';
+import { LoggerService } from './logger.service';
 
 describe('CalculatorService', () => {
   let calculator: CalculatorService;
@@ -6,7 +8,16 @@ describe('CalculatorService', () => {
 
   beforeEach(() => {
     loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
-    calculator = new CalculatorService(loggerSpy);
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        {
+          provide: LoggerService,
+          useValue: loggerSpy,
+        }
+      ]
+    })
+    calculator = TestBed.get(CalculatorService);
   });
 
   it('should add two numbers', () => {
