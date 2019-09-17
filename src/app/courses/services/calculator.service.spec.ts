fe0a967a5e78ac476c43b3165db320f091ed0660
Calvin Durrant <calvindurramt@Calvins-MacBook-Pro.local>
2019-09-17 12:59:22 +0200

    calculator service test: implemented before each

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,20 +1,25 @@
 import { CalculatorService } from './calculator.service';
-import { LoggerService } from './logger.service';
 
 describe('CalculatorService', () => {
+  let calculator: CalculatorService;
+  let loggerSpy: any;
+
+  beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+    calculator = new CalculatorService(loggerSpy);
+  });
+
   it('should add two numbers', () => {
-    const logger = jasmine.createSpyObj('LoggerService', ['log']);
-    const calculator = new CalculatorService(logger);
     const result = calculator.add(2, 2);
 
     expect(result).toBe(4);
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
   it('should subtract two numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
     const result = calculator.subtract(2, 2);
 
     expect(result).toBe(0, 'unexpected subtraction result');
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 });
